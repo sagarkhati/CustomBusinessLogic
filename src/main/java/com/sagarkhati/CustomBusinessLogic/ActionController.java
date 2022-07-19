0deaaa143487c8c6785514f1ca5bc458d4565ecd
sagarkhati <sonu06sagar@gmail.com>
2022-07-19 09:40:59 +0530

    adding try-catch at controller in addNumbers endpoint

--- src/main/java/com/sagarkhati/CustomBusinessLogic/ActionController.java
+++ src/main/java/com/sagarkhati/CustomBusinessLogic/ActionController.java
@@ -11,16 +11,23 @@ class ActionController {
 	AddResult handler(@RequestBody addNumbersArgs actionArgs) {
 		// logic
 		
-		Iterable<Integer> numbers = actionArgs.getnumbers();
-		
-		int sum = 0;
-		for (Integer i : numbers) {
-			sum += i;
+		try {
+			Iterable<Integer> numbers = actionArgs.getnumbers();
+			
+			int sum = 0;
+			for (Integer i : numbers) {
+				sum += i;
+			}
+
+			AddResult result = new AddResult();
+			result.setsum(sum);
+
+			return result;
+		} catch (Exception e) {
+			System.out.println(e);
+			
+			return new AddResult();
 		}
-
-		AddResult result = new AddResult();
-		result.setsum(sum);
-
-		return result;
+		
 	}
 }
